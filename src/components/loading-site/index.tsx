f7a5e6348ae3cd676004f4bacb4e45d16310285a
azadov-azamat <azamatazadov112@gmail.com>
2024-10-24 09:19:53 +0500

    fix

--- src/components/loading-site/index.tsx
+++ src/components/loading-site/index.tsx
@@ -3,7 +3,7 @@
 export default function Component({setLoading}: any) {
 
     // const dispatch = useAppDispatch();
-
+    console.log(setLoading)
     return (
         <div className="relative w-full h-screen bg-white flex items-center justify-center">
             Loading....
