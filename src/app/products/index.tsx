79c930391ea74a13a7e7d86447e7db7491325460
azadov-azamat <azamatazadov112@gmail.com>
2025-02-03 19:34:43 +0500

    changes

--- src/app/products/index.tsx
+++ src/app/products/index.tsx
@@ -5,18 +5,25 @@ import {getNotifications, getProducts} from "../../redux/reducers/variable.ts";
 import {useLocation} from "react-router-dom";
 import qs from 'qs';
 import { getBuckets } from "../../redux/reducers/bucket.ts";
-// import {getBucketCount} from "../../redux/reducers/bucket";
+import { motion } from "framer-motion";
+import { BiX } from "react-icons/bi";
+import { useTranslation } from "react-i18next";
 
 export default function Controller() {
 
     const dispatch = useAppDispatch()
     const location = useLocation()
-
+    const { t } = useTranslation();
     const {products} = useAppSelector(state => state.variables)
     const {auth} = useAppSelector(state => state.auth)
     const query = qs.parse(location.search, {ignoreQueryPrefix: true})
 
     const [view, setView] = React.useState('list')
+    const [isVisible, setIsVisible] = React.useState(false);
+
+    const handleToggle = () => {
+      setIsVisible(!isVisible);
+    };
     
     const toggleView =(item: string) => setView(item);
     
@@ -52,7 +59,7 @@ export default function Controller() {
             <div className="px-3 bg-white">
                 <TopMenuSection/>
                 <div className={'py-6'}>
-                    <FilterRowSection view={view} toggle={toggleView}/>
+                    <FilterRowSection view={view} toggle={toggleView} toggleFilter={handleToggle}/>
                 </div>
             </div>
             <div className={'flex flex-wrap max-350:flex-auto gap-4 justify-between px-3'}>
@@ -66,6 +73,31 @@ export default function Controller() {
                     />
                 )}
             </div>
+            {isVisible && (
+        <motion.div
+          initial={{ y: "100%" }} // Start position (bottom)
+          animate={{ y: "0%" }} // End position (top)
+          exit={{ y: "100%" }} // Exit position (back to bottom)
+          transition={{ duration: 0.5, ease: "easeInOut" }}
+          className="fixed inset-0 z-50 bg-white"
+        >
+            <div className={'sticky top-0 z-10 pb-4 pt-5 bg-white flex justify-center items-center'}>
+                <div onClick={handleToggle} className={'absolute left-4 cursor-pointer'}>
+                    <BiX size={24}/>
+                </div>
+                <h2 className="text-lg font-semibold">{t ('filters')}</h2>
+            </div>
+            <div className="absolute bottom-0 left-0 right-0 bg-[#f2f3f2] rounded-t-3xl top-16 grid justify-between grid-cols-2 gap-2 px-4 py-6 overflow-y-auto">
+                
+                <div className="fixed bottom-0 left-0 right-0 px-3 py-6">
+                    <button onClick={handleToggle} className="relative bg-primary-blurple text-xs text-white w-full py-4 rounded-[90px] flex justify-center
+                    items-center gap-4 uppercase">
+                        {t ('find-by-filter')}
+                    </button>
+                </div>
+            </div>
+        </motion.div>
+      )}
         </>
     )
 }
--- src/components/filter-row/index.tsx
+++ src/components/filter-row/index.tsx
@@ -5,12 +5,12 @@ import {
     FullViewIcon
 } from "../../assets/icons"
 
-export default function Component({toggle, view}: {view: string; toggle: any}) {
+export default function Component({toggle, view, toggleFilter}: {view: string; toggle: any; toggleFilter: any;}) {
     const {t} = useTranslation();
-
+  
     return (
         <div className="flex items-center justify-between w-full">
-            <div className="flex items-center space-x-2">
+            <div className="flex items-center space-x-2" onClick={toggleFilter}>
                 <span className="text-sm font-semibold uppercase">{t ('filter-sort')}</span>
                 <SortToolIcon/>
             </div>
